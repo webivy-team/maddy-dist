5bd10287405ee8bc540c2df7a606340d86ee1c1a
Svante Richter <svante.richter@gmail.com>
2023-10-16 13:09:35 +0200

    Fix test and user creation

--- npm/maddy-dist/index.js
+++ npm/maddy-dist/index.js
@@ -1,4 +1,4 @@
-import { spawn } from "node:child_process";
+import { spawn, spawnSync } from "node:child_process";
 import { dirname, resolve } from "node:path";
 
 import { fileURLToPath } from "node:url";
@@ -11,6 +11,49 @@ const binPath = resolve(
   `maddy-dist-${process.platform}-${process.arch}`,
   /^win/.test(process.platform) ? "maddy.exe" : "maddy",
 );
+
+export const maddyCmd = (...args) => {
+  const proc = spawnSync(
+    binPath,
+    args,
+    {
+      env: {
+        MADDY_CONFIG: resolve(process.cwd(), 'maddy.conf'),
+        CURRENT_DIR: process.cwd(),
+        ...process.env,
+      },
+    }
+  )
+  console.log(proc.stdout.toString())
+  console.error(proc.stderr.toString())
+}
+
+export const createUser = (email, password) => new Promise(async res => {
+  const proc = spawn(
+    binPath,
+    ['creds', 'create', email],
+    {
+      env: {
+        MADDY_CONFIG: resolve(process.cwd(), 'maddy.conf'),
+        CURRENT_DIR: process.cwd(),
+        ...process.env,
+      },
+    }
+  )
+  // TODO: How to get when maddy is ready for input?
+  await new Promise(res => setTimeout(() => res(), 100))
+  proc.stdin.write(password)
+  proc.stdin.write("\n")
+  proc.stdout.on('data', async (data) => {
+    console.log(`stdout: "${data}"`);
+  });
+  proc.stderr.on('data', (data) => {
+    console.error(`stderr: "${data}"`);
+  });
+  proc.on("close", (code) => {
+    res()
+  });
+})
 
 export default async (env = {}) => {
   const proc = await new Promise((pResolve, reject) => {
--- tests/tests/server-startup.test.js
+++ tests/tests/server-startup.test.js
@@ -1,9 +1,43 @@
 import assert from "node:assert";
 import test from "node:test";
+import Imap from "node-imap";
 
-import maddy from "maddy-dist"
+import maddy, { maddyCmd, createUser } from "maddy-dist"
+
+const getInbox = () => new Promise((res, rej) => {
+  var imap = new Imap({
+    user: 'postmaster@localhost',
+    password: 'test',
+    host: 'localhost',
+    port: 1993,
+    tls: false
+  });
+  imap.once('ready', function () {
+    imap.openBox('INBOX', true, (err, box) => {
+      if (err) rej(err)
+      imap.end()
+      res(box)
+    })
+  })
+
+  imap.once('error', function (err) {
+    rej(err)
+  })
+
+  imap.once('end', function () {
+    console.log('Connection ended')
+  })
+
+  imap.connect()
+})
 
 test("Server startup", async () => {
   const server = await maddy()
+  await createUser('postmaster@localhost', 'test')
+  maddyCmd('imap-acct', 'create', 'postmaster@localhost')
+  const box = await getInbox()
+  assert(box.name === 'INBOX')
+  // Wait for imap to have ended its session to avoid a conn reset
+  await new Promise(res => setTimeout(() => res(), 10))
   server.stop();
 })
